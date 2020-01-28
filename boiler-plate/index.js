37dfd1259ae479c6569446067565403eb75f449a
jaem1n207 <jaem1n207@naver.com>
2020-01-28 15:30:12 +0900

    [FIX] URI_error

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(`config.mongoURI`, {
+  .connect(config.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
