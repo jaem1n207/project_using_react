55f18449aee0544a7330cceea0ae20f2444994ba
jaem1n207 <jaem1n207@naver.com>
2020-01-30 23:18:09 +0900

    [ADD] VIdeoUploadFrom Template

---
+++ boilerplate-mern-stack-master/client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Typography, Button, Form, message, Input, Icon } from "antd";
+
+const { TextArea } = Input;
+const { Title } = Typography;
+
+function VideoUploadPage() {
+  return (
+    <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
+      <div style={{ textAlign: "center", marginBottom: "2rem" }}>
+        <Title level={2}>Upload Video</Title>
+      </div>
+
+      <Form onSubmit>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          {/* Drop zone */}
+
+          {/* Tunmnail */}
+          <div>
+            <img src alt></img>
+          </div>
+        </div>
+        <br />
+        <br />
+        <Input onChange value />
+        <br />
+        <br />
+        <label>Description</label>
+        <TextArea onChange value></TextArea>
+        <br />
+        <br />
+
+        <selection onChange>
+          <option key value></option>
+        </selection>
+        <selection onChange>
+          <option key value></option>
+        </selection>
+        <Button type="primary" size="large" onClick>
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+export default VideoUploadPage;
