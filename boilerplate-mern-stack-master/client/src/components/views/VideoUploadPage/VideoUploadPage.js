a42bdbdbc80024a1e5eb2e91935b5550f572bcb9
jaem1n207 <jaem1n207@naver.com>
2020-02-01 23:33:55 +0900

    [ADD] videoRoute

--- boilerplate-mern-stack-master/client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ boilerplate-mern-stack-master/client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Typography, Button, Form, message, Input, Icon } from "antd";
 import Dropzone from "react-dropzone";
+import Axios from "axios";
+import { response } from "express";
 
 const { TextArea } = Input;
 const { Title } = Typography;
@@ -36,6 +38,22 @@ function VideoUploadPage() {
     setCategory(e.currentTarget.value);
   };
 
+  const onDrop = files => {
+    let formData = new FormData();
+    const config = {
+      header: { "content-type": "multipart/form-data" }
+    };
+    formData.append("file", files[0]);
+
+    Axios.post("/api/video/uploadfiles", formData, config).then(response => {
+      if (response.data.success) {
+        console.log(response.data);
+      } else {
+        alert("비디오 업로드를 실패했습니다.");
+      }
+    });
+  };
+
   return (
     <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
       <div style={{ textAlign: "center", marginBottom: "2rem" }}>
@@ -45,7 +63,7 @@ function VideoUploadPage() {
       <Form onSubmit>
         <div style={{ display: "flex", justifyContent: "space-between" }}>
           {/* Drop zone */}
-          <Dropzone onDrop multiple maxSize>
+          <Dropzone onDrop={onDrop} multiple={false} maxSize={1000000000}>
             {({ getRootProps, getInputProps }) => (
               <div
                 style={{
