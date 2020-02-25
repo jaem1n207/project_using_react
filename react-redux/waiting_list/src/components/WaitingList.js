3461cb9d7b5ebbe512866f3e2280aec1202ba99d
jaem1n207 <jaem1n207@naver.com>
2020-02-25 22:02:25 +0900

    [ADD] components

---
+++ react-redux/waiting_list/src/components/WaitingList.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./WaitingList.css";
+
+const WaitingItem = ({ text, entered, onEnter, onLeave }) => {
+  return (
+    <li>
+      <div className={`text ${entered ? "entered" : ""}`}>{text}</div>
+      <div className="buttons">
+        <button onClick={onEnter}>입장</button>
+        <button onClick={onLeave}>나감</button>
+      </div>
+    </li>
+  );
+};
+
+const WaitingList = ({ waitingList, onEnter, onLeave }) => {
+  return (
+    <div className="WaitingList">
+      <h2>대기자 명단</h2>
+      <form>
+        <input />
+        <button>등록</button>
+      </form>
+      <ul>
+        <WaitingItem text="뜨뜨" entered />
+        <WaitingItem text="도라에몽" />
+        <WaitingItem text="짱구" />
+      </ul>
+    </div>
+  );
+};
+
+export default WaitingList;
