d858c4dbf414c606ca827e5339fed0077cfafe65
jaem1n207 <jaem1n207@naver.com>
2020-04-01 23:19:46 +0900

    [ADD] gugudan

---
+++ react-games/gugudan/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useRef } from "react";
+
+function App() {
+  const [value, setValue] = useState("");
+  const [first, setFirst] = useState(Math.ceil(Math.random() * 9));
+  const [second, setSecond] = useState(Math.ceil(Math.random() * 9));
+  const [result, setResult] = useState(0);
+  const inputEl = useRef(null);
+
+  const onSubmitForm = e => {
+    e.preventDefault();
+    if (parseInt(value) === first * second) {
+      setResult("정답");
+      setFirst(Math.ceil(Math.random() * 9));
+      setSecond(Math.ceil(Math.random() * 9));
+      setValue("");
+      inputEl.current.focus();
+    } else {
+      setResult("땡");
+      setValue("");
+      inputEl.current.focus();
+    }
+  };
+
+  const onChange = e => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <>
+      <div>
+        {first} 곱하기 {second}은(는)?
+      </div>
+      <form onSubmit={onSubmitForm}>
+        <input ref={inputEl} type="number" value={value} onChange={onChange} />
+        <button>입력</button>
+      </form>
+      <div>{result}</div>
+    </>
+  );
+}
+
+export default App;
