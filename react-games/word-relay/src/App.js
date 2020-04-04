71f0b6fa4cac33ef41dc0749cc57450ff5c58242
jaem1n207 <jaem1n207@naver.com>
2020-04-04 23:56:23 +0900

    [ADD] wordrelay

--- react-games/word-relay/src/App.js
+++ react-games/word-relay/src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState("");
+  const [word, setWord] = useState("짱구");
+  const inputEl = useRef(null);
+
+  const onSubmitForm = e => {
+    e.preventDefault();
+    if (word[word.length - 1] === value[0]) {
+      setResult("정답!");
+      setWord(value);
+      setValue("");
+      inputEl.current.focus();
+    } else {
+      setResult("땡!");
+      setValue("");
+      inputEl.current.focus();
+    }
+  };
+
+  const onChangeValue = e => {
+    setValue(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>{word}</div>
+      <form onSubmit={onSubmitForm}>
+        <input ref={inputEl} value={value} onChange={onChangeValue} />
+        <button>클릭</button>
+      </form>
+      <div>{result}</div>
+    </>
   );
 }
 
