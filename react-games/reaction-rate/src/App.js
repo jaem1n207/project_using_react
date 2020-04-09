abaf92507b78a0be58b93eeae316ade95e170c78
jaem1n207 <jaem1n207@naver.com>
2020-04-09 22:58:20 +0900

    [UPDATE] reaction.rate function

--- react-games/reaction-rate/src/App.js
+++ react-games/reaction-rate/src/App.js
@@ -1,14 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./App.css";
 
 function App() {
   const [state, setState] = useState("waiting");
   const [message, setMessage] = useState("클릭해서 시작하세요!");
   const [result, setResult] = useState([]);
+  const timeout = useRef(null);
+  const startTime = useRef(0);
+  const endTime = useRef(0);
 
-  const onClickScreen = () => {};
+  const onClickScreen = () => {
+    if (state === "waiting") {
+      setState("ready");
+      setMessage("초록색이 되면 클릭하세요.");
+      timeout.current = setTimeout(() => {
+        setState("now");
+        setMessage("지금 클릭");
+        startTime.current = new Date();
+      }, Math.floor(Math.random() * 1000) + 2000);
+    } else if (state === "ready") {
+      clearTimeout(timeout.current);
+      setState("waiting");
+      setMessage("너무 성급하시군요! 초록색이 된 후에 클릭하세요.");
+    } else if (state === "now") {
+      endTime.current = new Date();
+      setState("waiting");
+      setMessage("클릭해서 시작하세요.");
+      setResult((prevState) => {
+        return [...prevState, endTime.current - startTime.current];
+      });
+    }
+  };
 
-  const renderAverage = () => {};
+  const onReset = () => {
+    setResult([]);
+  };
+
+  const renderAverage = () => {
+    return result.length === 0 ? null : (
+      <>
+        <div>평균 시간: {result.reduce((a, c) => a + c) / result.length}ms</div>
+        <button onClick={onReset}>리셋</button>
+      </>
+    );
+  };
 
   return (
     <>
