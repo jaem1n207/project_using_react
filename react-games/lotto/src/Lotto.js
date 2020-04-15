94822a40d180938131ce6fff7d5e9820a75870d1
jaem1n207 <jaem1n207@naver.com>
2020-04-15 19:02:07 +0900

    [UPDATE] useEffect

--- react-games/lotto/src/Lotto.js
+++ react-games/lotto/src/Lotto.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Ball from "./Ball";
 
 function getWinNumbers() {
@@ -6,7 +6,7 @@ function getWinNumbers() {
     .fill()
     .map((v, i) => i + 1);
   const shuffle = [];
-  while (RTCIceCandidate.length > 0) {
+  while (candidate.length > 0) {
     shuffle.push(
       candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]
     );
@@ -17,12 +17,52 @@ function getWinNumbers() {
 }
 
 const Lotto = () => {
+  const [winNumbers, setWinNumbers] = useState(getWinNumbers());
+  const [winBalls, setWinBalls] = useState([]);
+  const [bonus, setBonus] = useState(null);
+  const [redo, setRedo] = useState(false);
+  const timeouts = useRef([]);
+
+  useEffect(() => {
+    console.log("useEffect");
+
+    for (let i = 0; i < winNumbers.length - 1; i++) {
+      timeouts.current[i] = setTimeout(() => {
+        setWinBalls((prevBalls) => [...prevBalls, winNumbers[i]]);
+      }, (i + 1) * 1000);
+    }
+    timeouts.current[6] = setTimeout(() => {
+      setBonus(winNumbers[6]);
+      setRedo(true);
+    }, 7000);
+    return () => {
+      timeouts.current.forEach((v) => {
+        clearTimeout(v);
+      });
+    };
+  }, [timeouts.current]); // 빈 배열이면 componentDidMount와 동일 / 배열에 요소가 있으면 componentDidMount랑 componentDidUpdate 둘 다 수행
+
+  const onClickRedo = () => {
+    console.log("onClickRedo");
+
+    setWinNumbers(getWinNumbers());
+    setWinBalls([]);
+    setBonus(null);
+    setRedo(false);
+    timeouts.current = [];
+  };
+
   return (
     <>
       <div>당첨 숫자</div>
-      <div id="결과창">{}</div>
+      <div id="결과창">
+        {winBalls.map((v) => (
+          <Ball key={v} number={v} />
+        ))}
+      </div>
       <div>보너스!</div>
-      {}
+      {bonus && <Ball number={bonus} />}
+      {redo && <button onClick={onClickRedo}>한 번 더!</button>}
     </>
   );
 };
